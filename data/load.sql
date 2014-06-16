98caa65605f4fe58d8ed87b1aec23789845e85ee
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-06-16 17:29:10 -0400

    Need explicit UTF-8 for some reason

--- data/load.sql
+++ data/load.sql
@@ -7,22 +7,22 @@ CREATE TABLE verbs (
     verb_id     INT NOT NULL AUTO_INCREMENT,
     table_id    INT NOT NULL,
     verb_root   VARCHAR(16) NOT NULL,
-    PRIMARY KEY(verb_id));
+    PRIMARY KEY(verb_id)) CHARSET utf8;
 CREATE TABLE tenses (
     tense_id        INT NOT NULL AUTO_INCREMENT,
     tense_name      VARCHAR(16) NOT NULL,
     table_id_start  INT NOT NULL,
     table_id_end    INT NOT NULL,
-    PRIMARY KEY(tense_id));
+    PRIMARY KEY(tense_id)) CHARSET utf8;
 CREATE TABLE tables (
     table_id    INT NOT NULL AUTO_INCREMENT,
     table_rule  VARCHAR(1000) NOT NULL,
-    PRIMARY KEY(table_id));
+    PRIMARY KEY(table_id)) CHARSET utf8;
 
-LOAD DATA LOCAL INFILE "verbs.csv" INTO TABLE verbs
+LOAD DATA LOCAL INFILE "verbs.csv" INTO TABLE verbs CHARSET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES
 (table_id, verb_root);
-LOAD DATA LOCAL INFILE "tenses.csv" INTO TABLE tenses
+LOAD DATA LOCAL INFILE "tenses.csv" INTO TABLE tenses CHARSET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA LOCAL INFILE "tables.csv" INTO TABLE tables
+LOAD DATA LOCAL INFILE "tables.csv" INTO TABLE tables CHARSET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
